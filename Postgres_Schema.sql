5d1cd85e5e91bef50587b781d203c87588f0444f
eglulu6 <eglulu6@gmail.com>
2021-03-18 20:00:07 -0400

    adding postgres schema file

---
+++ Postgres_Schema.sql
@@ -0,0 +1,32 @@
+﻿CREATE TABLE "quote" (
+    --COL---------TYPE-------NULL
+    "quote_id"    int       NOT NULL,
+    "author_name" text      NOT NULL,
+    "quote_text"  text      NOT NULL,
+        ---SET KEYS
+        PRIMARY KEY ("quote_id"),
+        FOREIGN KEY("author_name")
+            REFERENCES "author"("author_name")
+    );
+
+CREATE TABLE "author" (
+    --COL------------------TYPE-------NULL
+    "author_id"           serial    NOT NULL,
+    "author_name"         text      NOT NULL,
+    "birth_date"          int       NOT NULL,
+    "birth_place"         text      NOT NULL,
+    "author_description"  text      NOT NULL,
+        ---SET KEYS
+        PRIMARY KEY ("author_id")
+    );
+
+CREATE TABLE "tags" (
+    ---COL------TYPE----NULL
+    "quote_id"  int    NOT NULL,
+    "tags"      text   NOT NULL,
+        ---SET KEYS
+        PRIMARY KEY ("quote_id"),
+        FOREIGN KEY ("quote_id")
+            REFERENCES "quotes"("quote_id")
+    );
+
