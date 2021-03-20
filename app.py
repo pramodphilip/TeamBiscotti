b3669f17dbf3ec16c45679910b88329605a477e1
pramodphilip <75338454+pramodphilip@users.noreply.github.com>
2021-03-20 18:33:37 -0400

    last tag finished, edits to app routes to improve api output

--- app.py
+++ app.py
@@ -2,10 +2,12 @@
 import numpy as np
 import sqlalchemy
 import json
+import pandas as pd
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func, inspect, desc
 from flask import Flask, jsonify, render_template
+from operator import itemgetter
 # postgres pasword
 from config import postgres_password as password
 
@@ -68,7 +70,7 @@ def quote():
     # Total # of quotes
     tot_num_quotes = session.query(Quotes).group_by(Quotes.quote_id).count() #100
 
-    # Quesry postgres tables to get data on all quotes
+    # Queries postgres tables to get data on all quotes
     all_quotes = engine.execute(f"\
                                 SELECT quote.quote_text, quote.author_name,\
                                     STRING_AGG (tags.tags_list,', ' \
@@ -83,7 +85,7 @@ def quote():
     quote_list = []
     for i in range(0,len(all_quotes)):
         quote_dict = {}
-        quote_dict["quote"] = all_quotes[i][0]
+        quote_dict["quote"] = all_quotes[i][0].replace('\u201d','').replace('\u201c','')
         quote_dict['auth'] = all_quotes[i][1]
         quote_dict['tags'] = all_quotes[i][2]
         quote_list.append(quote_dict)
@@ -91,7 +93,7 @@ def quote():
     # add total number of Quotes and the quote_list to one dict for easier return statement
     quote_json = [f"Total Number of Quotes: {tot_num_quotes}", quote_list]
 
-    return jsonify(Quotes=quote_list, Number_of_Qoutes=tot_num_quotes)
+    return jsonify(Quotes=quote_list, Number_of_Quotes=tot_num_quotes)
     
 #############################################################################
 
@@ -99,8 +101,10 @@ def quote():
 
 @app.route("/api/v1.0/author")
 def author():
-    # start new session
-    session = Session(bind=engine)
+    session = Session(bind=engine) 
+
+    # Total # of authors
+    tot_num_authors = session.query(Author).group_by(Author.author_id).count() #100
 
     # run grouping query for all the author info
     all_author = engine.execute(f"\
@@ -115,7 +119,7 @@ def author():
         INNER JOIN author \
         USING (author_name) \
         GROUP BY quote.author_name, author.author_name, author.description, author.birth_date, author.birth_place, quote.quote_text").fetchall()
-    # close session          
+
     session.close()
 
     # initialize empty list to hold all dicts  
@@ -127,11 +131,11 @@ def author():
         if all_author[a][0] != all_author[a-1][0]:
             # initialize empty dict to hold only auth info
             all_author_dict = {}
-            all_author_dict["name"] = all_author[a][0],
-            all_author_dict["description"] = all_author[a][1], 
-            all_author_dict["birth_date"] = all_author[a][2], 
-            all_author_dict["birth_place"] = all_author[a][3], 
-            all_author_dict["num_quotes"] = all_author[a][5],
+            all_author_dict["author_name"] = all_author[a][0]
+            all_author_dict["description"] = all_author[a][1].replace('\u2013','').replace('\u00e9','')
+            all_author_dict["birth_date"] = all_author[a][2] 
+            all_author_dict["birth_place"] = all_author[a][3] 
+            all_author_dict["num_quotes"] = all_author[a][5]
 
             # initialize empty list to hold quotes and tags data
             quote_tag_list = []
@@ -140,18 +144,20 @@ def author():
             for q in range(0,len(all_author[a])):
                 # initialize empty dict for quotes and tags data 
                 quote_tag_dict={}
-                quote_tag_dict["quote"] = all_author[q][4]
+                quote_tag_dict["quote_text"] = all_author[q][4].replace('\u201d','').replace('\u201c','')
                 quote_tag_dict["tags"] = all_author[q][6]
                 # append quotes and tags data to list outside of loop layer 2 
                 quote_tag_list.append(quote_tag_dict)
 
             # add all the quote tags from layer 2 into dict
-            all_author_dict["quote_tags"] = quote_tag_list
+            all_author_dict["quotes"] = quote_tag_list
 
             # append auth info dict to list outside of all the loops
-            all_author_list.append(all_author_dict)    
+            all_author_list.append(all_author_dict)  
+
+    true_author_dict = {'total':tot_num_authors,'detail':all_author_list}      
     
-    return jsonify(all_author_list)
+    return jsonify(true_author_dict)
 
 #############################################################################
 
@@ -159,8 +165,6 @@ def author():
 
 @app.route("/api/v1.0/author/<author_name>")
 def one_author(author_name):
-    # start new session
-    session = Session(bind=engine)
 
     # run grouping query for all the author info
     one_author = engine.execute(f"\
@@ -175,9 +179,6 @@ def one_author(author_name):
         WHERE author_name = '{author_name}' \
         GROUP BY quote.author_name, author.description, author.birth_date, author.birth_place, quote.quote_text").fetchall()
     
-    # close session          
-    session.close()
-
     # add if statement for returning no value message
     if len(one_author)==0:
         one_auth_dict = {"name" : author_name,
@@ -187,7 +188,7 @@ def one_author(author_name):
     else:                   
 
         # Create a dict with the static values     
-        one_auth_dict = {"name" : one_author[0][0],
+        one_auth_dict = {"author_name" : one_author[0][0],
                         "description" : one_author[0][1], 
                         "birth_date" : one_author[0][2], 
                         "birth_place" : one_author[0][3], 
@@ -199,12 +200,12 @@ def one_author(author_name):
         # for loop to get the related tags and quote info into sing dict   
         for i in range(0,len(one_author)):
             quote_tag_dict={}
-            quote_tag_dict["quote"] = one_author[i][4]
+            quote_tag_dict["text"] = one_author[i][4]
             quote_tag_dict["tags"] = one_author[i][6]
             quote_tag_list.append(quote_tag_dict)
 
         # add list with little dicts to big dict   
-        one_auth_dict['quote_tags']=quote_tag_list
+        one_auth_dict['quotes']=quote_tag_list
 
     return jsonify(one_auth_dict)
 
@@ -230,11 +231,18 @@ def tags():
         GROUP BY tags.tags_list, quote.quote_id \
         ORDER BY tags_list  ASC").fetchall()
 
+    all_tag_lists = engine.execute(f"select quote.quote_text, \
+            STRING_AGG (tags.tags_list, ', '  ORDER BY quote.author_name) tags \
+            from quote, tags \
+            Group by quote.author_name, quote.quote_text")
     # close session          
     session.close()
     
     # create a df to isolate tags & quote info
     df = pd.DataFrame(all_tags, columns=['tag', 'quotes'])
+
+    # create a second df to isolate quote info and list of tags for each quote
+    df_2 = pd.DataFrame(all_tag_lists, columns=['quotes','tag_lists'])
 
     # isolate only the unique tags
     tags = df.tag.unique()
@@ -245,12 +253,17 @@ def tags():
     # for loop to iterate the unique tags list and return the df values for those 
     for tag in tags:
         quotes = list(df[df['tag'] == tag]['quotes'])
-        # create dict for the tag value, the count of all quotes, and then the actual quotes
-        tag_dict = {'tag': tag, 'count': len(quotes), 'quotes': quotes}
-        # append each dict to the list of all tags
+        #quote_tags_list = []
+        #for quote in quotes:
+        #    tag_list = df_2[df['quotes'] == quote]['tag_lists']
+        #    quote_tlist_dict = {'text':quote,'tags':tag_list}
+        #    quote_tags_list.append(quote_tlist_dict)
+        tag_dict = {'name': tag, 'number_of_quotes': len(quotes), 'quotes': quotes}
         all_tag_list.append(tag_dict)
-        
-    return jsonify(all_tag_list)
+            
+    true_tags_dict = {'count':tot_num_tags,'details':all_tag_list}
+
+    return jsonify(true_tags_dict)
 
 #############################################################################
 
@@ -258,8 +271,6 @@ def tags():
 
 @app.route("/api/v1.0/tags/<tag>")
 def one_tag(tag):
-    # start new session
-    session = Session(bind=engine)
 
     # run grouping query for all the author info
     one_tag = engine.execute(f"\
@@ -274,9 +285,6 @@ def one_tag(tag):
         GROUP BY tags.tags_list, quote.quote_id \
         ORDER BY tags_list  ASC").fetchall()
 
-    # close session          
-    session.close()
-
     # add if statement for returning no value message
     if len(one_tag)==0:
         one_tag_dict = {'tag' : tag , 'value' : 'Does not exist'}
@@ -287,7 +295,7 @@ def one_tag(tag):
         one_tag_list = []
 
         # initialize dict for one tag info
-        one_tag_dict = {'tag' : one_tag[0][0], 'count' : one_tag[0][2]}
+        one_tag_dict = {'name_of_tag' : one_tag[0][0], 'count' : one_tag[0][2]}
 
         # for loop to get all quote info
         for q in range(len(one_tag)):
@@ -296,7 +304,7 @@ def one_tag(tag):
             one_tag_list.append(quote_dict)
             
         # add one tag list to the bigger dict of all quotes for single tag    
-        one_tag_dict['quote_tags']=one_tag_list    
+        one_tag_dict['quotes']=one_tag_list    
 
     return jsonify(one_tag_dict)
 
@@ -304,10 +312,33 @@ def one_tag(tag):
 
 #	6. This route will display the information about top10 tags
 
-#@app.route("/api/v1.0/top10tags")
-#def top_ten_tag():
+@app.route("/api/v1.0/top10tags")
+def top_ten_tag():
 
+    # run grouping query for all the author info
+    all_tags = engine.execute(f"\
+    SELECT tags.tags_list, quote.quote_text \
+    FROM tags \
+    INNER JOIN quote \
+    USING (quote_id) \
+    GROUP BY tags.tags_list, quote.quote_id \
+    ORDER BY tags_list  ASC").fetchall()
 
+    df = pd.DataFrame(all_tags, columns=['tag', 'quotes'])
+
+    tags = df.tag.unique()
+
+    all_tag_list = []
+    for tag in tags:
+        quotes = list(df[df['tag'] == tag]['quotes'])
+        tag_dict = {'name_of_tag': tag, 'quote_count': len(quotes)}
+        all_tag_list.append(tag_dict)
+
+        
+    top_10 = sorted(all_tag_list,key=itemgetter('quote_count'), reverse=True)
+    top_10 = top_10[0:10]
+
+    return jsonify(top_10)
 #############################################################################
 
 engine.dispose()
